4af48a3639c80d76a811135f2cc77a385d939223
zafer siar konyar <siarkonyar@gmail.com>
2021-06-27 19:13:01 +0300

    navbar-done

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,5 +1,16 @@
-$(document).ready(function(){
-	$('.hamburger').click(function(){
-		$(this).toggleClass('open');
-	});
+const menuBtn = document.querySelector('.hamburger');
+let menuOpen = false;
+menuBtn.addEventListener('click', () => {
+	if(!menuOpen) {
+		menuBtn.classList.add('open');
+		menuOpen = true;
+	} else {
+		menuBtn.classList.remove('open');
+		menuOpen = false;
+	}
+});
+
+const navbar = document.querySelector('.navbar');
+menuBtn.addEventListener('click', () => {
+	navbar.classList.toggle('show');
 });
