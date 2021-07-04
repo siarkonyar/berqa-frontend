b5316d66f6f9c5dae8896670115d7bbdd2958635
zafer siar konyar <siarkonyar@gmail.com>
2021-07-04 20:06:05 +0300

    conact-us.html done

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,16 +1,20 @@
-const menuBtn = document.querySelector('.hamburger');
+const menuBtn = document.querySelector(".hamburger");
 let menuOpen = false;
-menuBtn.addEventListener('click', () => {
-	if(!menuOpen) {
-		menuBtn.classList.add('open');
-		menuOpen = true;
-	} else {
-		menuBtn.classList.remove('open');
-		menuOpen = false;
-	}
+menuBtn.addEventListener("click", () => {
+  if (!menuOpen) {
+    menuBtn.classList.add("open");
+    menuOpen = true;
+  } else {
+    menuBtn.classList.remove("open");
+    menuOpen = false;
+  }
 });
 
-const navbar = document.querySelector('.navbar');
-menuBtn.addEventListener('click', () => {
-	navbar.classList.toggle('show');
+const navbar = document.querySelector(".navbar");
+menuBtn.addEventListener("click", () => {
+  navbar.classList.toggle("show");
+});
+
+$(document).ready(function () {
+  $(".dropdown-toggle").dropdown();
 });
