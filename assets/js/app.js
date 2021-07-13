81d5c8e5fc85bf3c9a3f6a10844f151ef8a84d55
zafer siar konyar <siarkonyar@gmail.com>
2021-07-13 16:08:52 +0300

    index.html

--- assets/js/app.js
+++ assets/js/app.js
@@ -22,14 +22,61 @@ $(document).ready(function () {
 const nextIcon = '<i class="bi bi-chevron-right"></i>';
 const prevIcon = '<i class="bi bi-chevron-left"></i>';
 
-$('.owl-carousel').owlCarousel({
-  navText:[
-    prevIcon,
-    nextIcon
-  ],
-  responsive: {
+$(document).ready(function () {
+  $("#home-products-carousel").owlCarousel({
+    loop: true,
+    margin: 10,
+    nav: true,
+    dots: false,
+    responsive: {
       0: {
-          items: 1
+        items: 1,
       },
-  }
-})
+      600: {
+        items: 2,
+      },
+      1000: {
+        items: 3,
+      },
+      1400: {
+        items: 4,
+      },
+    },
+    navText: [prevIcon, nextIcon],
+  });
+  $("#media-carousel").owlCarousel({
+    loop: true,
+    margin: 10,
+    nav: false,
+    dots: true,
+    responsive: {
+      0: {
+        items: 1,
+      },
+    },
+  });
+  $("#media-item").owlCarousel({
+    loop: true,
+    margin: 10,
+    nav: true,
+    dots: false,
+    responsive: {
+      0: {
+        items: 1,
+      },
+    },
+    navText: [prevIcon, nextIcon],
+  });
+  $("#home-media-carousel").owlCarousel({
+    loop: true,
+    margin: 10,
+    nav: true,
+    dots: false,
+    responsive: {
+      0: {
+        items: 1,
+      }
+    },
+    navText: [prevIcon, nextIcon],
+  });
+});
